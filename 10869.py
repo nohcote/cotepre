efa29f5856787df0b0aa37b6f2bd37884b8cdb4f
nohcote <nohcote@gmail.com>
2022-07-12 22:14:57 +0900

    +-*/%

---
+++ 10869.py
@@ -0,0 +1,6 @@
+inp = input().split(' ')
+print(int(inp[0])+int(inp[1]))
+print(int(inp[0])-int(inp[1]))
+print(int(inp[0])*int(inp[1]))
+print(int(int(inp[0])/int(inp[1])))
+print(int(inp[0])%int(inp[1]))
