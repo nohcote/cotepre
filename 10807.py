6110d9cd39a6ab2dc9400a3a0aa2e4ab3cf9dd4e
nohcote <nohcote@gmail.com>
2022-07-19 06:43:42 +0900

    count ints better

--- 10807.py
+++ 10807.py
@@ -5,10 +5,4 @@ n = int(sys.stdin.readline().rstrip())
 arr = sys.stdin.readline().rstrip().split()
 v = int(sys.stdin.readline().rstrip())
 
-count = 0
-
-for i in range(0, len(arr)):
-    if int(arr[i]) == v:
-        count += 1
-        
-print(count)
+print(arr.count(str(v)))
