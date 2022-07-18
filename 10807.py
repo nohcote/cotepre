89daa411f020dcbf3d6dff12a4a29e268ad68d61
nohcote <nohcote@gmail.com>
2022-07-19 07:07:47 +0900

    count ints more better

--- 10807.py
+++ 10807.py
@@ -3,6 +3,6 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 arr = sys.stdin.readline().rstrip().split()
-v = int(sys.stdin.readline().rstrip())
+v = sys.stdin.readline().rstrip()
 
-print(arr.count(str(v)))
+print(arr.count(v))
