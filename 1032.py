be501ef9b88d41cee6f18650216abe3e738a40bc
nohcote <nohcote@gmail.com>
2022-07-26 21:43:59 +0900

    update samechar

--- 1032.py
+++ 1032.py
@@ -3,10 +3,9 @@ import sys
 
 n = int(sys.stdin.readline())
 a = list(sys.stdin.readline())
-a_len = len(a)
-for i in range(n - 1):
+for i in range(n-1):
     b = list(sys.stdin.readline())
-    for j in range(a_len):
+    for j in range(len(a)):
         if a[j] != b[j]:
             a[j] = '?'
 print(''.join(a))
