08e814c93db7616261597f3ac8c9a4569fa0a95e
nohcote <109136189+nohcote@users.noreply.github.com>
2022-08-07 08:32:00 +0900

    get len of str w processed croatian alphabet

--- 2941.py
+++ 2941.py
@@ -1,9 +1,5 @@
 arr = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-
 temp = input()
-total = 0
 for a in arr:
-    total += temp.count(a)
     temp = temp.replace(a, " ")
-temp = temp.replace(" ", "")
-print(total+len(temp))
+print(len(temp))
