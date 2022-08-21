f69226394250b1e321897ca2c406d6d2a749b8f8
nohcote <109136189+nohcote@users.noreply.github.com>
2022-08-21 19:59:25 +0900

    find how many inscribed dots

---
+++ 1002.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+
+for _ in range(int(sys.stdin.readline())):
+    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
+    
+    # find inscribed circle
+    d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    if d==0 and r1==r2: print(-1)
+    elif abs(r1-r2) == d or r1+r2 == d: print(1)
+    elif abs(r1-r2) < d < r1+r2: print(2)
+    else: print(0)
