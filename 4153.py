7ede811af3a41e39f666bd3cfa80bc0eede8c766
nohcote <109136189+nohcote@users.noreply.github.com>
2022-08-21 16:32:38 +0900

    pitagoras

---
+++ 4153.py
@@ -0,0 +1,12 @@
+import sys
+
+
+while True:
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a==b==c==0:
+        break
+
+    if (max(a,b,c)**2)*2-a**2-b**2-c**2 == 0:
+        print("right")
+    else:
+        print("wrong")
