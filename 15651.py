f21b2fcde3f16f7bac54a8216c61ebec52623c27
nohcote <109136189+nohcote@users.noreply.github.com>
2022-08-28 23:57:07 +0900

    dfs get nums with replacement

---
+++ 15651.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+
+
+N, M = map(int, sys.stdin.readline().split())
+arr = deque()
+
+def dfs():
+    if len(arr) == M:
+        [print(e, end=" ") for e in arr]
+        print()
+        return
+    else:
+        for i in range(1, N+1):
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+
+dfs()
